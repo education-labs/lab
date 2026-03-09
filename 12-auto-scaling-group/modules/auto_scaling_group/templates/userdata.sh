eb7fcb53ea5eb075e9c9197e99ed56013119a6e9
education-labs <116854704+education-labs@users.noreply.github.com>
2026-03-09 16:29:25 +0900

    Clean up userdata.sh by removing unused code
    Removed redundant IP address retrieval and logging.

--- 12-auto-scaling-group/modules/auto_scaling_group/templates/userdata.sh
+++ 12-auto-scaling-group/modules/auto_scaling_group/templates/userdata.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-IP_ADDR=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-
-sudo echo "<p>SERVER IP : $IP_ADDR</p>" >> /data/html/index.html
-
-#!/bin/bash
-
 # 1. 디렉토리 존재 확인 및 생성
 sudo mkdir -p /data/html
 
